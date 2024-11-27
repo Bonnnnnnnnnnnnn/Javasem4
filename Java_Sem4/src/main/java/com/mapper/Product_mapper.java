2becaf1cdd3ae81a27e41c4dda53a29e3f1e3352
trongdo123 <nguyendotrong2003@gmail.com>
2024-11-27 14:00:27 +0700

    MergeProduct_mapper

--- Java_Sem4/src/main/java/com/mapper/Product_mapper.java
+++ Java_Sem4/src/main/java/com/mapper/Product_mapper.java
@@ -2,11 +2,9 @@ package com.mapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 import org.springframework.jdbc.core.RowMapper;
 
-import com.customer.repository.FeedbackRepository;
 import com.models.Product;
 import com.utils.Views;
 
@@ -22,25 +20,19 @@ public class Product_mapper implements RowMapper<Product> {
         item.setStatus(rs.getString(Views.COL_PRODUCT_STATUS));
         item.setPrice(rs.getDouble(Views.COL_PRODUCT_PRICE));
         item.setWarranty_period(rs.getInt(Views.COL_PRODUCT_WARRANTY_PERIOD));
-        item.setStatus(rs.getString(Views.COL_PRODUCT_STATUS));
-        item.setBrandName(rs.getString("brand_name"));
-        item.setUnit_name(rs.getString("unit_name"));
-        item.setCategoryName(rs.getString("category_name"));
-        try {
-            FeedbackRepository feedbackRepo = MapperHelper.getFeedbackRepo();
-            if (feedbackRepo != null) {
-                int productId = item.getId();
-                item.setAverageRating(feedbackRepo.calculateAverageRating(productId));
-                item.setFeedbacks(feedbackRepo.getProductFeedbacks(productId));
-                item.setTotalFeedbacks(item.getFeedbacks().size());
-            }
-        } catch (Exception e) {
-            System.err.println("Error getting feedback data: " + e.getMessage());
-            item.setAverageRating(0.0);
-            item.setFeedbacks(new ArrayList<>());
-            item.setTotalFeedbacks(0);
-        }
+        item.setUnit_id(rs.getInt(Views.COL_PRODUCT_UNIT_ID));
+        item.setHeight(rs.getInt(Views.COL_PRODUCT_HEIGHT));
+        item.setWeight(rs.getInt(Views.COL_PRODUCT_WELGHT));
+        item.setLength(rs.getInt(Views.COL_PRODUCT_LENGTH));
+        item.setHeight(rs.getInt(Views.COL_PRODUCT_HEIGHT));
+        
 
+		item.setBrandName(rs.getString("brand_name"));
+		item.setCategoryName(rs.getString("category_name"));
+		item.setUnit_name(rs.getString("unit_name"));
+
+        
         return item;
     }
 }
+
